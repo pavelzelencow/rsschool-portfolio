88315b6b0d9c254e15457e32407e14413868f8a5
pavelzelencow <a2621086@iclod.com>
2022-01-31 15:42:29 +0300

    fix: style and index js

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,24 +1,4 @@
-    console.log(
-        'блок <header> +6',
-        'секция hero +6',
-        'секция skills +6',
-        'секция portfolio +6',
-        'секция video +6',
-        'секция price +6',
-        'секция contacts +6',
-        'блок <footer> +6',
-        'нет полосы прокрутки при ширине страницы от 1440рх до 768рх +5',
-        'нет полосы прокрутки при ширине страницы от 768рх до 480рх +5',
-        'нет полосы прокрутки при ширине страницы от 480рх до 320рх +5',
-        'при ширине страницы 768рх панель навигации скрывается, появляется бургер-иконка +2',
-        'при нажатии на бургер-иконку справа плавно появляется адаптивное меню, бургер-иконка изменяется на крестик +4',
-        'высота адаптивного меню занимает всю высоту экрана. При ширине экрана 768-620рх вёрстка меню соответствует макету, когда экран становится уже, меню занимает всю ширину экрана +4',
-        'при нажатии на крестик адаптивное меню плавно скрывается уезжая за правую часть экрана, крестик превращается в бургер-иконку +4',
-        'бургер-иконка, которая при клике превращается в крестик, создана при помощи css-анимаций без использования изображений +2',
-        'при клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, крестик превращается в бургер-иконку +4'
-    );
-
-let x = document.querySelector(".menu-btn");
+  let x = document.querySelector(".menu-btn");
 
 x.addEventListener("click", myFunction);
 
@@ -54,3 +34,129 @@ function closeMenu(event) {
       // здесь код, удаляющий класс `'open'` у гамбургер-иконки и у меню
     }
   }
+
+  function theme() {
+    const toggleTheme = document.querySelector('.menu-btn__theme')
+    let el = document.documentElement
+
+    toggleTheme.addEventListener('click', () => {
+        if(el.hasAttribute('data-theme')) {
+            el.removeAttribute('data-theme')
+            localStorage.removeItem('theme')
+        } else {
+            el.setAttribute('data-theme', 'white')
+            localStorage.setItem('theme', 'white')
+        }
+    })
+
+    if(localStorage.getItem('theme') !== null) {
+        el.setAttribute('data-theme', 'white')
+    }
+}
+
+theme()
+
+const i18Obj = {
+  'en': {
+    'skills': 'Skills',
+    'portfolio': 'Portfolio',
+    'video': 'Video',
+    'price': 'Price',
+    'contacts': 'Contacts',
+    'hero-title': 'Alexa Rise',
+    'hero-text': 'Save sincere emotions, romantic feelings and happy moments of life together with professional photographer Alexa Rise',
+    'hire': 'Hire me',
+    'skill-title-1': 'Digital photography',
+    'skill-text-1': 'High-quality photos in the studio and on the nature',
+    'skill-title-2': 'Video shooting',
+    'skill-text-2': 'Capture your moments so that they always stay with you',
+    'skill-title-3': 'Rotouch',
+    'skill-text-3': 'I strive to make photography surpass reality',
+    'skill-title-4': 'Audio',
+    'skill-text-4': 'Professional sounds recording for video, advertising, portfolio',
+    'winter': 'Winter',
+    'spring': 'Spring',
+    'summer': 'Summer',
+    'autumn': 'Autumn',
+    'price-description-1-span-1': 'One location',
+    'price-description-1-span-2': '120 photos in color',
+    'price-description-1-span-3': '12 photos in retouch',
+    'price-description-1-span-4': 'Readiness 2-3 weeks',
+    'price-description-1-span-5': 'Make up, visage',
+    'price-description-2-span-1': 'One or two locations',
+    'price-description-2-span-2': '200 photos in color',
+    'price-description-2-span-3': '20 photos in retouch',
+    'price-description-2-span-4': 'Readiness 1-2 weeks',
+    'price-description-2-span-5': 'Make up, visage',
+    'price-description-3-span-1': 'Three locations or more',
+    'price-description-3-span-2': '300 photos in color',
+    'price-description-3-span-3': '50 photos in retouch',
+    'price-description-3-span-4': 'Readiness 1 week',
+    'price-description-3-span-5': 'Make up, visage, hairstyle',
+    'order': 'Order shooting',
+    'contact-me': 'Contact me',
+    'send-message': 'Send message'
+  },
+  'ru': {
+    'skills': 'Навыки',
+    'portfolio': 'Портфолио',
+    'video': 'Видео',
+    'price': 'Цены',
+    'contacts': 'Контакты',
+    'hero-title': 'Алекса Райс',
+    'hero-text': 'Сохраните искренние эмоции, романтические переживания и счастливые моменты жизни вместе с профессиональным фотографом',
+    'hire': 'Пригласить',
+    'skill-title-1': 'Фотография',
+    'skill-text-1': 'Высококачественные фото в студии и на природе',
+    'skill-title-2': 'Видеосъемка',
+    'skill-text-2': 'Запечатлите лучшие моменты, чтобы они всегда оставались с вами',
+    'skill-title-3': 'Ретушь',
+    'skill-text-3': 'Я стремлюсь к тому, чтобы фотография превосходила реальность',
+    'skill-title-4': 'Звук',
+    'skill-text-4': 'Профессиональная запись звука для видео, рекламы, портфолио',
+    'winter': 'Зима',
+    'spring': 'Весна',
+    'summer': 'Лето',
+    'autumn': 'Осень',
+    'price-description-1-span-1': 'Одна локация',
+    'price-description-1-span-2': '120 цветных фото',
+    'price-description-1-span-3': '12 отретушированных фото',
+    'price-description-1-span-4': 'Готовность через 2-3 недели',
+    'price-description-1-span-5': 'Макияж, визаж',
+    'price-description-2-span-1': 'Одна-две локации',
+    'price-description-2-span-2': '200 цветных фото',
+    'price-description-2-span-3': '20 отретушированных фото',
+    'price-description-2-span-4': 'Готовность через 1-2 недели',
+    'price-description-2-span-5': 'Макияж, визаж',
+    'price-description-3-span-1': 'Три локации и больше',
+    'price-description-3-span-2': '300 цветных фото',
+    'price-description-3-span-3': '50 отретушированных фото',
+    'price-description-3-span-4': 'Готовность через 1 неделю',
+    'price-description-3-span-5': 'Макияж, визаж, прическа',
+    'order': 'Заказать съемку',
+    'contact-me': 'Свяжитесь со мной',
+    'send-message': 'Отправить'
+  }
+}
+
+
+const ru = document.querySelector('.ru');
+const en = document.querySelector('.en');
+
+function getTranslate(langs){
+  const text = document.querySelectorAll("[data-i18]")
+  text.forEach(item => {
+    item.testContent = i18Obj[langs][item.dataset.i18]
+    item.placeholder = i18Obj[langs][item.dataset.i18]
+    console.log(langs, cl0b);
+  })
+}
+
+ru.addEventListener('click', (event) =>{
+  langs = event.target.dataset.ru;
+  if (event.target.classList.contains('menu-btn__lang')){
+    getTranslate(langs)
+  }
+});
+
+
